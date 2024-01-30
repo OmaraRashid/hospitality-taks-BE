5a96ae312d16e228adb8301ed02ab20d5ad0b0c4
OmaraRashid <omararashed1@gmail.com>
2024-01-30 23:30:26 +0200

    Add project files.

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HOSPITALITY.Application;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace hospitality_taks_BE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+        private readonly BookingService _bookingService;
+
+        public BookingController(BookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet("GetReminderEmails")]
+        public IActionResult GetReminderEmails(DateTime date, int restaurantId)
+        {
+            var reminderEmails = _bookingService.GetReminderEmails(date, restaurantId);
+            return Ok(reminderEmails);
+        }
+    }
+
+}
